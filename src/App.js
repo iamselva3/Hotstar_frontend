e1d2ae4bef0243439fd83d03f3391ab92b512a68
Selva <imselva1512@gmail.com>
2025-11-13 12:24:25 +0530

    Missing log

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ function App() {
   const [user, setuser ] = useState("");
   const [page, setPage] = useState("/login"); 
   const [cart, setCart] = useState([]);
-conso.los(page);
+console.log(page);
   return (
     <div className="App">
 
